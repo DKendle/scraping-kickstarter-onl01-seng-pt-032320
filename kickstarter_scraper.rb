06c4895f10e22656c838821095e740ed53073eec
Dina Kendle <dina.kendle@gmail.com>
2020-04-28 18:45:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ kickstarter = Nokogiri::HTML(html)
 # title: project.css("h2.bbcard_name strong a").text
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-#location: 
+#location: project.css("span.location-name").text
 binding.pry
 end
 
